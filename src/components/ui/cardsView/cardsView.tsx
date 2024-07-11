113f9742df030cf254da47e4be6d394db8e83088
antonina220590 <tonya2290@gmail.com>
2024-07-11 19:54:09 +0300

    feat: update loader

--- src/components/ui/cardsView/cardsView.tsx
+++
@@ -1,7 +0,0 @@
-// import Cards from '../cards/cards';
-// import { IPlanet } from '../../utils/interface';
-
-// function ViewCards(planet: IPlanet) {
-//   return <Cards {...planet} />;
-// }
-// export default ViewCards;
--- src/components/ui/search-page/search_page.tsx
+++ src/components/ui/search-page/search_page.tsx
@@ -4,21 +4,32 @@ import styles from '../cards/cards.module.css';
 import Input from '../input/input';
 import Cards from '../cards/cards';
 import { IPlanet } from '../../utils/interface';
+import Spinner from '../spinner/spinner';
+import { getSearch } from '../../api/api';
 
 function SearchPage() {
-  const BASE_URL = 'https://swapi.dev/api/';
   const [planets, setPlanets] = useState<IPlanet[] | null>([]);
   const [valueV, setValueV] = useState<string>(
     localStorage.getItem('ATSearch') || ''
   );
+  const [isLoading, setIsLoading] = useState(false);
 
   const getMyData = (value: string) =>
     setValueV(localStorage.getItem('ATSearch') || value);
 
   useEffect(() => {
-    fetch(`${BASE_URL}/planets/?search=${valueV}`)
-      .then((res) => res.json())
-      .then((data) => setPlanets(data.results));
+    const fetchPlanets = async () => {
+      try {
+        setIsLoading(true);
+        const fetched = await getSearch(valueV);
+        setPlanets(fetched);
+      } catch (error) {
+        <p>error</p>;
+      } finally {
+        setIsLoading(false);
+      }
+    };
+    fetchPlanets();
   }, [valueV]);
 
   return (
@@ -29,23 +40,29 @@ function SearchPage() {
         </div>
       </div>
       <div className={style.cardsWrapper}>
-        {planets?.map((planet) => {
-          return (
-            <div className={styles.cardContainer} key={planet.name}>
-              <Cards
-                name={planet.name}
-                climate={planet.climate}
-                diameter={planet.diameter}
-                gravity={planet.gravity}
-                orbital_period={planet.orbital_period}
-                population={planet.population}
-                rotation_period={planet.rotation_period}
-                terrain={planet.terrain}
-                url={planet.url}
-              />
-            </div>
-          );
-        })}
+        {isLoading ? (
+          <Spinner />
+        ) : (
+          <div className={style.commonWrapper}>
+            {planets?.map((planet) => {
+              return (
+                <div className={styles.cardContainer} key={planet.name}>
+                  <Cards
+                    name={planet.name}
+                    climate={planet.climate}
+                    diameter={planet.diameter}
+                    gravity={planet.gravity}
+                    orbital_period={planet.orbital_period}
+                    population={planet.population}
+                    rotation_period={planet.rotation_period}
+                    terrain={planet.terrain}
+                    url={planet.url}
+                  />
+                </div>
+              );
+            })}
+          </div>
+        )}
       </div>
     </>
   );
