69029402f1db94234b26abac57cc9e981901bb86
antonina220590 <tonya2290@gmail.com>
2024-07-24 21:25:38 +0300

    feat: change button to checkboxes for theme changer

--- src/themeButton.tsx
+++ src/themeButton.tsx
@@ -3,15 +3,33 @@ import { ThemeContext } from './context/themeContext';
 import style from './App.module.css';
 
 function ThemeBtn() {
-  const { darkTheme, setTheme } = useContext(ThemeContext);
+  const { setTheme } = useContext(ThemeContext);
   return (
-    <button
-      type="button"
-      onClick={() => setTheme((prev) => (prev === 'dark' ? 'light' : 'dark'))}
-      className={`${darkTheme === 'dark' ? style.darkBtn : style.lightBtn}`}
-    >
-      Change Theme
-    </button>
+    <div className={style.themeBtnBox}>
+      <label htmlFor="darkBtn" className={style.themeLabel}>
+        Dark Theme{' '}
+        <input
+          id="darkBtn"
+          className={style.radioBtn}
+          type="radio"
+          name="themeBtn"
+          value="darkTheme"
+          onClick={() => setTheme('dark')}
+          defaultChecked
+        />{' '}
+      </label>
+      <label htmlFor="lightBtn" className={style.themeLabel}>
+        Light Theme{' '}
+        <input
+          id="lightBtn"
+          className={style.radioBtn}
+          type="radio"
+          name="themeBtn"
+          value="lightTheme"
+          onClick={() => setTheme('light')}
+        />{' '}
+      </label>
+    </div>
   );
 }
 
