53cb879826dd46ae32401f6d10c30b06baeed600
antonina220590 <tonya2290@gmail.com>
2024-08-14 09:02:22 +0300

    feat: add links to the main page

--- src/components/ui/mainPage/mainPage.tsx
+++ src/components/ui/mainPage/mainPage.tsx
@@ -1,5 +1,13 @@
+import { Link } from 'react-router-dom';
+
 function MainPage() {
-  return <div>Hello! I am Main Page</div>;
+  return (
+    <div>
+      <Link to="forms1">Uncontrolled Form</Link>
+      <br />
+      <Link to="forms2">React Hooks Forms</Link>
+    </div>
+  );
 }
 
 export default MainPage;
